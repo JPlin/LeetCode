7950bddf6f29ab49a0187eac5aafef2a44804cbb
JPlin <1037062323@qq.com>
2017-12-30 18:48:18 +0800

    740

---
+++ 740.py
@@ -0,0 +1,17 @@
+import collections
+class Solution(object):
+    def deleteAndEarn(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        cnt = collections.Counter(nums)
+        maxn = max(nums + [0])
+        dp = [0] * (maxn + 10)
+        for x in range(1 , maxn + 1):
+            dp[x] = max(dp[x-1] , dp[x-2] + cnt[x] * x)
+        return dp[maxn]
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.deleteAndEarn([3,4,2]))
